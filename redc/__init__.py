7ced6fcfefcdb4d1627e337ea4eb534fd0b2d103
AYMENJD <53928879+AYMENJD@users.noreply.github.com>
2025-04-21 07:00:36 +0300

    Update version to 0.1.4.

--- redc/__init__.py
+++ redc/__init__.py
@@ -1,21 +1,21 @@
-from .callbacks import StreamCallback, ProgressCallback
+from . import utils
+from .callbacks import ProgressCallback, StreamCallback
 from .client import Client
 from .codes import HTTPStatus
 from .exceptions import HTTPError
 from .response import Response
-from . import utils
 
 __all__ = [
-    "Client",
-    "Response",
-    "HTTPError",
-    "HTTPStatus",
-    "StreamCallback",
-    "ProgressCallback",
     "utils",
+    "ProgressCallback",
+    "StreamCallback",
+    "Client",
+    "HTTPStatus",
+    "HTTPError",
+    "Response",
 ]
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __copyright__ = "Copyright (c) 2025 RedC, AYMENJD"
 __license__ = "MIT License"
 
