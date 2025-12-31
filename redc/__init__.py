135db77f4e08f780be3d0e8769cfed38880e7ac1
AYMENJD <53928879+AYMENJD@users.noreply.github.com>
2026-01-01 00:26:22 +0300

    Update copyright year.
    skip-checks:true

--- redc/__init__.py
+++ redc/__init__.py
@@ -16,7 +16,7 @@ __all__ = [
 ]
 
 __version__ = "0.1.9"
-__copyright__ = "Copyright (c) 2025 RedC, AYMENJD"
+__copyright__ = "Copyright (c) 2025-2026 RedC, AYMENJD"
 __license__ = "MIT License"
 
 VERSION = __version__
